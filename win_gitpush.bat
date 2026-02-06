4db988a0e2f0aec0158cbba7da24081a8153aa75
priyodas <priyodas0177@gmail.com>
2026-02-07 01:25:27 +0600

    all oke

--- win_gitpush.bat
+++ win_gitpush.bat
@@ -1,5 +1,15 @@
 @echo off
+
+:ask
+set /p msg=Enter commit message: 
+
+if "%msg%"=="" (
+    echo Commit message cannot be empty!
+    goto ask
+)
+
 git add .
-git commit -m %1
+git commit -m "%msg%"
 git push origin main
+
 pause
